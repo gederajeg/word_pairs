9152c1e1030de6a5750ea482b4481de7cbb26d65
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-09-21 10:19:34 +1000

    separate the word_pair_match() function into a new file

--- word_pairs.R
+++ word_pairs.R
@@ -1,35 +1,3 @@
-#' @title word_pair_match
-#' @description Extracting sentences that contain a pair of words.
-#'     The current version of the code accepts a character vector whose elements represent text of sentences (as in the corpus files from the Leipzig Corpora Collection).
-#' @param corpus A character vector of sentences.
-#' @param word_pairs A character vector whose elements are regular expressions for each of the word pairs.
-#'     The current version of the code accepts a two-word pair.
-#'
-#' @return A character vector of the matched sentences in which the searched word pair appears together.
-#'
-#' @examples
-#' word_pairs <- c("\\bhari\\b", "\\bdepan\\b")
-#' corpus_path <- "/Users/Primahadi/Documents/Corpora/_corpusindo/Leipzig Corpora/ind_news_2008_300K-sentences.txt"
-#' corpus <- readLines(corpus_path, encoding = "UTF-8")
-#' word_pair_out <- word_pair_match(corpus, word_pairs)
-#' @references Rajeg, Gede Primahadi Wijaya. (2018). word_pair_match: An R function to retrieve word pair in sentences of the (Indonesian) Leipzig Corpora.
-word_pair_match <- function(corpus, word_pairs) {
-  word_pairs_backref <- paste("(", word_pairs, ")", sep = "")
-  word_pairs_rgx <- paste(word_pairs_backref, collapse = "(.+?)")
-  matches <- grep(word_pairs_rgx, corpus, perl = TRUE, value = TRUE)
-  
-  backref_elements <- unlist(strsplit(word_pairs_rgx, "[()]"))
-  backref_elements <- backref_elements[nzchar(backref_elements)]
-  backref_seq <- seq(length(backref_elements))
-  backref_with_words <- grep("[a-zA-Z]+", backref_elements, perl = T)
-  backref_rgx <- ifelse(backref_seq %in% backref_with_words, 
-                        paste("<w", backref_seq, ">\\", backref_seq, "</w", backref_seq, ">", sep = ""), 
-                        paste("\\", backref_seq, sep = ""))
-  backref_rgx <- paste(backref_rgx, collapse = "")
-  matches_tag <- gsub(word_pairs_rgx, backref_rgx, matches, perl = TRUE)
-  return(matches_tag)
-}
-
 #' @title Detect word pairs in text
 #' 
 #' @description \code{word_pairs} searches for the occurrences of a pair of words in sentences. These words can be separated by intervening strings (viz. other in-between words), which is also handled by \code{word_pairs}.
