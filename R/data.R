16fb19fded8bcecb8b3338b68cb1823f1d9e1dbf
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2019-02-24 16:50:57 +1100

    add documentation for bola_corpus_text

--- R/data.R
+++ R/data.R
@@ -4,3 +4,11 @@
 #' @format A character vector of 25,000 elements of sentences.
 #' @source \url{http://wortschatz.uni-leipzig.de/en/download}
 "my_leipzig_sample"
+
+
+#' Corpus data non-Leipzig
+#'
+#' @description This is an example input data whose line does not correspond to one sentence as in the Leipzig Corpora.
+#' @format A character vector of 18 elements that do not all correspond to one sentence.
+#' @source from BOLA NEWS.com article entitled "Nostalgia Fergie dan Beckham" posted on 16 February 2010
+"bola_corpus_text"
